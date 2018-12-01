575a2faa6d162da3811f3f498ed58c22b37d1f0b
Tran Minh Nguyen Duc <nguyen.minh@live.de>
2018-12-01 19:19:40 +0100

    weird video effect

---
+++ DiscoverYourPowerLevel/Assets/Scripts/VideoEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoEffect : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        transform.parent = EffectManager.Instance.EffectCanvas;
+        
+        var pos = transform.position;
+        pos.y = Random.Range(0, 500);
+        transform.position = pos;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        transform.position += new Vector3(100, 0, 0) * Time.deltaTime;
+
+        if (transform.position.x >= 2000)
+            Destroy(gameObject);
+    }
+}
