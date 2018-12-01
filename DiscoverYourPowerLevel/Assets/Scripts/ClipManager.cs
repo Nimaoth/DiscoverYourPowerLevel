88bb843470859df79ccbd71c38e9979e84fc0b51
Nico Otremba <nico.otremba@mmi-media.de>
2018-12-01 01:03:00 +0100

    stuff

--- DiscoverYourPowerLevel/Assets/Scripts/ClipManager.cs
+++ DiscoverYourPowerLevel/Assets/Scripts/ClipManager.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class ClipManager : MonoBehaviour {
 
+    public static ClipManager Instance;
 
     public Clip[] Clips;
 
     private int CurrentClipIndex = -1;
     private Clip currentClip = null;
+
+    public AudioSource ClipAudioSource;
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    private void Start() {
+        ClipAudioSource = GetComponent<AudioSource>();
+    }
 
     private void Update() {
         if (currentClip == null) {
--- DiscoverYourPowerLevel/Assets/Scripts/GameManager.cs
+++ DiscoverYourPowerLevel/Assets/Scripts/GameManager.cs
@@ -9,7 +9,6 @@ public class GameManager : MonoBehaviour {
     public Player Player1;
     public Player Player2;
 
-
     private void Awake() {
         Instance = this;
         DontDestroyOnLoad(gameObject);
@@ -23,6 +22,15 @@ public class GameManager : MonoBehaviour {
     }
     
     void Update () {
+        if (Player1.PowerLevel < 0) {
+            Player1.PowerLevel = 0;
+        }
         
+        if (Player2.PowerLevel < 0) {
+            Player2.PowerLevel = 0;
+        }
+
+        Debug.Log($"p1: {Player1.PowerLevel}, p2: {Player2.PowerLevel}");
+
     }
 }
