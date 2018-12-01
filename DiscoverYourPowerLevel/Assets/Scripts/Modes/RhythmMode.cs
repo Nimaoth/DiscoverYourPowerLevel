f39837c5831dfe78c2684a955e31e1faf65aac60
er <ga87jun@in.tum.de>
2018-12-01 06:09:14 +0100

    as

---
+++ DiscoverYourPowerLevel/Assets/Scripts/Modes/RhythmMode.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Custom/Modes/RhythmMode")]
+public class RhythmMode : Mode{
+    public string Player1Key;
+    public string Player2Key;
+
+    public int LossPerHit;
+    public int GainPerHit;
+
+    public float tactTime;
+    public float timingAccuracy;
+    public float tickOffset;
+    public float timer;
+
+    public AudioClip testSound;
+    public override void Start()
+    {
+        base.Start();
+        timer = tickOffset;
+    }
+    public override void OnUpdate(float time)
+    {
+        if(timer <= 0)
+        {
+            //AudioSource.PlayClipAtPoint(testSound, Vector3.zero);
+            timer = tactTime;
+        }
+        if (Input.GetKeyDown(Player1Key) && (timer < timingAccuracy || timer > tactTime - timingAccuracy))
+        {
+            player1.PowerLevel += GainPerHit;
+        } else
+            if (Input.GetKeyDown(Player1Key) && !(timer < timingAccuracy || timer > tactTime - timingAccuracy))
+            {
+                player1.PowerLevel -= LossPerHit;
+            }
+
+        if (Input.GetKeyDown(Player2Key) && (timer < timingAccuracy || timer > tactTime - timingAccuracy))
+        {
+            player2.PowerLevel += GainPerHit;
+        } else
+            if (Input.GetKeyDown(Player2Key) && !(timer < timingAccuracy || timer > tactTime - timingAccuracy))
+            {
+                player2.PowerLevel -= LossPerHit;
+            }
+
+        //player1.PowerLevel -= (int)(LossPerSecond * time);
+        //player2.PowerLevel -= (int)(LossPerSecond * time);
+        timer -= time;
+    }
+}
