a9da529d1cdae495e651c1a1a11c19549b17de65
Nico Otremba <nico.otremba@mmi-media.de>
2018-12-01 05:02:02 +0100

    test effect

--- DiscoverYourPowerLevel/Assets/Scripts/EffectManager.cs
+++ DiscoverYourPowerLevel/Assets/Scripts/EffectManager.cs
@@ -4,30 +4,36 @@ using UnityEngine;
 
 public class EffectManager : MonoBehaviour {
 
+    public static EffectManager Instance;
+
 
     public GameObject[] NegEffects;
     public GameObject[] PosEffects;
     public GameObject[] SuperPosEffects;
 
-    private void PlayEffect(int effect, GameObject[] EffectType)
+    private void Awake() {
+        Instance = this;
+    }
+
+    public void PlayEffect(int effect, GameObject[] EffectType)
     {
         GameObject prefab = EffectType[effect];
         Instantiate(prefab);
     }
 
-    private void PlayNegEffct()
+    public void PlayNegEffect()
     {
         GameObject prefab = NegEffects[Random.Range(0, NegEffects.Length - 1)];
         Instantiate(prefab);
     }
 
-    private void PlayPosEffct()
+    public void PlayPosEffect()
     {
         GameObject prefab = PosEffects[Random.Range(0, PosEffects.Length - 1)];
         Instantiate(prefab);
     }
 
-    private void PlaySuperPosEffct()
+    public void PlaySuperPosEffect()
     {
         GameObject prefab = SuperPosEffects[Random.Range(0, SuperPosEffects.Length - 1)];
         Instantiate(prefab);
