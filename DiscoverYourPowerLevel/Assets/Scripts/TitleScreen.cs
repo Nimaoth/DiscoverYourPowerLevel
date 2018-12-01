5471291bab121c1da13199fd49d2a650dc4b4640
LAPTOP-NNVPUIJK\viola <viola.stumpf@mmi-media.de>
2018-12-01 04:35:53 +0100

    added update to shader in title screen

---
+++ DiscoverYourPowerLevel/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleScreen : MonoBehaviour {
+
+    public GameObject cylinder;
+    public GameObject empty;
+    Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+        rend = cylinder.GetComponent<Renderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rend.material.SetFloat("_Flow", empty.GetComponent<TitleScreenButton>().powerLevel);
+	}
+}
