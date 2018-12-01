411c65341ad91b292c9cb201794600a0209bd1c5
Albert Zach <albert.zach@tum.de>
2018-12-01 20:24:53 +0100

    First steps into screen shatter

---
+++ DiscoverYourPowerLevel/Assets/Scripts/ShatterRefraction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class ShatterRefraction : MonoBehaviour {
+
+    public Material shatterMaterial;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, shatterMaterial);
+    }
+}
