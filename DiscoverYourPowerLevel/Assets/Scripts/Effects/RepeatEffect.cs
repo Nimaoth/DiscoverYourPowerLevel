e126283ec44ede4e444a1b73599902c7bc26b9ca
Nico Otremba <nico.otremba@mmi-media.de>
2018-12-02 10:54:24 +0100

    effect

---
+++ DiscoverYourPowerLevel/Assets/Scripts/Effects/RepeatEffect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Custom/Effect/Repeat")]
+public class RepeatEffect : Effect
+{
+    public int Count;
+    public float Interval;
+
+    public bool AllAtOnce;
+
+    public Effect Effect;
+
+    public override void Spawn(Vector3 location)
+    {
+        GameManager.Instance.StartCoroutineEnum(SpanwObjects(location));
+    }
+
+    private IEnumerator SpanwObjects(Vector3 location) {
+        for (int i = 0; i < Count; i++) {
+            Effect.Spawn(location);
+
+            if (!AllAtOnce) {
+                yield return new WaitForSeconds(Interval);
+            }
+        }
+    }
+}
